3ddb4366a6264119f6d33312944697b662e7ece0
lindongwu <lindongwu@memect.co>
2018-03-07 19:04:36 +0800

    init: create-react-native-app

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components';
+// import IPFS from 'ipfs';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </Container>
+    );
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Sermover
+
+A moving server on your mobile phone.
+
+Scan QR code of a multihash to pin it, then you can serve it by IPFS.
